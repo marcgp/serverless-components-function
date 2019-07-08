4e23990703252745ae36fdb5d93feb08c9210e7f
Eslam A. Hefnawy <eahefnawy@gmail.com>
2019-07-08 17:07:39 +0300

    updated to use new context

--- serverless.js
+++ serverless.js
@@ -13,18 +13,11 @@ class Function extends Component {
    */
 
   async default(inputs = {}) {
-    this.ui.status('Deploying')
+    this.context.status('Deploying')
 
     const lambda = await this.load('@serverless/aws-lambda')
 
     const lambdaOutputs = await lambda(inputs)
-
-    this.ui.log()
-    this.ui.output('name', `       ${lambdaOutputs.name}`)
-    this.ui.output('description', `${lambdaOutputs.description}`)
-    this.ui.output('memory', `     ${lambdaOutputs.memory}`)
-    this.ui.output('timeout', `    ${lambdaOutputs.timeout}`)
-    this.ui.output('id', `         ${lambdaOutputs.arn}`)
 
     return lambdaOutputs
   }
@@ -36,7 +29,7 @@ class Function extends Component {
    */
 
   async remove(inputs = {}) {
-    this.ui.status('Removing')
+    this.context.status('Removing')
 
     const lambda = await this.load('@serverless/aws-lambda')
 
